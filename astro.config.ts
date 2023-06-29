406ae5c1803a81e771441870189756fb329679fa
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-06-29 14:06:13 +0100

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://astro-theme-cactus.netlify.app/",
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	compressHTML: true,
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+});
