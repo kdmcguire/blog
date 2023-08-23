3d35bdeb1cde4747cc0cf875e470e1af4dfc5a76
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-08-24 00:23:44 +0100

    fix(social): don't use h-full as a tailwind class

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -36,7 +36,7 @@ const ogOptions: SatoriOptions = {
 };
 
 const markup = (title: string, pubDate: string) => html`<div
-	tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]"
+	tw="flex flex-col w-full h-[100%] bg-[#1d1f21] text-[#c9cacc]"
 >
 	<div tw="flex flex-col flex-1 w-full p-10 justify-center">
 		<p tw="text-2xl mb-6">${pubDate}</p>
