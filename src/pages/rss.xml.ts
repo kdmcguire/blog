a08f44d65d1291c93686ef73a8459c62fc639aae
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-08-24 01:05:19 +0100

    feat(rss): full content in rss feed (#104)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { siteConfig } from "@/site-config";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export const get = async () => {
 	const posts = await getCollection("post");
@@ -10,9 +13,9 @@ export const get = async () => {
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.description,
 			pubDate: post.data.publishDate,
+			content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
 			link: `posts/${post.slug}`,
 		})),
 	});
