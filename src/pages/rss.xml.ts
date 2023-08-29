25b19022eea5c30b8f728e9a6bb75627ecc0b9a8
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-08-29 15:16:59 +0100

    feat(rss): improve rss contents

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,11 @@ import { getCollection } from "astro:content";
 import { siteConfig } from "@/site-config";
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+
+const parser = new MarkdownIt({
+	html: true,
+	xhtmlOut: true
+});
 
 export const get = async () => {
 	const posts = await getCollection("post");
@@ -12,11 +16,22 @@ export const get = async () => {
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
+		customData: '<language>en-uk</language>',
 		items: posts.map((post) => ({
+			title: post.data.title,
 			pubDate: post.data.publishDate,
-			content: sanitizeHtml(parser.render(post.body)),
-      ...post.data,
+			categories: post.data.tags,
+			description: post.data.description,
+			content: `<![CDATA[ ${sanitizeHtml(
+				parser.render(post.body),
+				{
+					exclusiveFilter: function (frame) {
+						return (frame.tag === 'p' && frame.text === 'import { Image } from "@astrojs/image/components";') || (frame.tag === 'p' && !frame.text.trim())
+					},
+				}
+			)} ]]>`,
 			link: `posts/${post.slug}`,
+			author: siteConfig.author,
 		})),
 	});
 };
