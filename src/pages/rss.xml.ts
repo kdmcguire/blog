1f4469fca74f5d0b224e2df43b71b5f5eb4ce029
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-08-29 17:29:45 +0100

    feat(rss): improve rss contents (#119)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,20 +3,36 @@ import { getCollection } from "astro:content";
 import { siteConfig } from "@/site-config";
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+
+const parser = new MarkdownIt({
+	html: true,
+	linkify: true,
+});
 
 export const get = async () => {
 	const posts = await getCollection("post");
 
 	return rss({
+		xmlns: { atom: 'http://www.w3.org/2005/Atom' },
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
+		customData: `<language>${siteConfig.lang}</language><atom:link href="https://kieran-mcguire.uk/rss.xml" rel="self" type="application/rss+xml" />`,
 		items: posts.map((post) => ({
+			title: post.data.title,
 			pubDate: post.data.publishDate,
-			content: sanitizeHtml(parser.render(post.body)),
-      ...post.data,
+			categories: post.data.tags,
+			description: post.data.description,
+			content: `<![CDATA[ ${sanitizeHtml(
+				parser.render(post.body),
+				{
+					exclusiveFilter: function (frame) {
+						return (frame.tag === 'p' && frame.text === 'import { Image } from "@astrojs/image/components";') || (frame.tag === 'p' && !frame.text.trim())
+					},
+				}
+			)} ]]>`,
 			link: `posts/${post.slug}`,
+			author: `kieran-mcguire@outlook.com (${siteConfig.author})`,
 		})),
 	});
 };
