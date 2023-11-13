8d128501cc5503cb628c1a20165e89857a24805d
Kieran McGuire <2109807+kdmcguire@users.noreply.github.com>
2023-11-13 00:39:19 +0000

    style(tabs): use tabs instead of spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,19 @@
 /** @type {import("@types/prettier").Options} */
 module.exports = {
-  plugins: [
-    "prettier-plugin-astro",
-    "prettier-plugin-tailwindcss" /* Must come last */,
-  ],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	printWidth: 80,
+	semi: true,
+	singleQuote: false,
+	useTabs: true,
+	plugins: [
+		"prettier-plugin-astro",
+		"prettier-plugin-tailwindcss" /* Must come last */,
+	],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
